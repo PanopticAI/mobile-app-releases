d041ea3d4765cb4d4938d7c8c5e63ae81b86edee
Eddie Lau <eddie@touchutility.com>
2023-04-11 14:18:57 +0800

    group the apps by name.

--- e6f94700f8f063a9fec8bbe81a5cb3fbc8cbb2d3/main.js
+++ e6f94700f8f063a9fec8bbe81a5cb3fbc8cbb2d3/main.js
@@ -1,8 +1,28 @@
+function groupByName(json) {
+  var output = []
+    for (var i = 0; i < json.length; i++){
+        var obj = json[i];
+        var found = output.find(element => element.name == obj.name);
+        if (found){
+            found.releases.push(obj)
+        }
+        else{
+            output.push({ 
+                "name": obj.name,
+                "releases":[obj]
+            });
+        }
+    }
+  return output
+  // return []
+}
+
 var app = new Vue({
   el: '#app',
   data: {
     config: {},
-    releases: []
+    releases: [],
+    mobileApps: []
   }
 });
 
@@ -22,5 +42,6 @@ $.ajax({
   mimeType: "application/json",
   success: function(data) {
     app.releases = data;
+    app.mobileApps = groupByName(data)
   }
 });
